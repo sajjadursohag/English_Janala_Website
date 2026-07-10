79907d4690152f18a065edd94b9d2c8dd11253ba
sajjadursohag <sajjadursub07@gmail.com>
2026-07-10 14:30:16 +0600

    added header, word container, index.js file

---
+++ script/index.js
@@ -0,0 +1,106 @@
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
+    .then((res) => res.json()) // promise of json data
+    .then((json) => displayLesson(json.data));
+};
+
+const removeActive = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+
+  lessonButtons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+};
+
+const loadLevelWord = (id) => {
+  // console.log(id);
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActive(); // remove all active class
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      // console.log(clickBtn);
+      clickBtn.classList.add("active"); // add active class
+      displayLevelWord(data.data);
+    });
+};
+
+// word dekhanor jonno
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla">
+    <img class="mx-auto " src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>`;
+    return;
+  }
+
+  //    {
+  //     "id": 83,
+  //     "level": 1,
+  //     "word": "Door",
+  //     "meaning": "দরজা",
+  //     "pronunciation": "ডোর"
+  // }
+
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
+     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl ">${word.word ? word.word : "শব্দ পাওয়া যায় নি "}</h2>
+        <p class=font-semibold>Meaning /Peonounciation</p>
+        <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায় নি"}"</div>
+        <div class="flex justify-between items-center">
+          <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>`;
+    wordContainer.append(card);
+  });
+};
+
+const loadWordDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  // console.log(url);
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (word) => {
+  console.log(word);
+  const detailsBox = document.getElementById("details-container");
+  detailsBox.innerHTML = "Hi am from JS";
+  document.getElementById("word_modal").showModal();
+};
+
+const displayLesson = (lessons) => {
+  // console.log(lessons);
+  // 1. get the container & empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+
+  // 2.get into every lessons
+  for (let lesson of lessons) {
+    //     3. create element
+    console.log(lessons);
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+    <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
+    <i class="fa-solid "></i>Lesson - ${lesson.level_no}
+    </button>
+     `;
+    //     4. append into container
+    levelContainer.append(btnDiv);
+  }
+};
+
+loadLessons();
